cab6d1e9a9721f5c83cbc3f4e23ad0de6df98dc1
mttetc <etchegaray.matthias@gmail.com>
2025-09-07 20:21:53 +0200

    refactor: #ABC-123 Ajouter des options de configuration pour la construction et le SSR dans vite.config.ts
    - Désactivation des sourcemaps pour la construction et l'esbuild pour améliorer les performances.
    - Ajout d'une gestion des avertissements de sourcemap dans les options de Rollup.
    - Configuration de SSR pour exclure certaines dépendances externes, améliorant ainsi la compatibilité avec Chakra UI.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignorer les warnings de sourcemap
+        if (warning.code === "SOURCEMAP_ERROR") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
+  ssr: {
+    noExternal: ["@chakra-ui/react", "@emotion/react", "@emotion/styled"],
+  },
+  esbuild: {
+    sourcemap: false,
+  },
 });
